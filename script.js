b4acff181e14ca07e7c8036f51a12efac8d5e42b
enochrx <drreayy@gmail.com>
2022-06-08 19:43:46 +0100

    added a challenge

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ const calcTempAmplitude = function (temps) {
 };
 
 console.log(calcTempAmplitude(temperatures));
+
+//Coding Challenge;
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+//Problem Solving framework
+//1. Usinf Test Data 1
+const temper1 = [17, 21, 23];
+const temper2 = [12, 5, -5, 0, 4];
+
+console.log(`${temper1[0]} ... ${temper1[1]} ... ${temper1[2]}`);
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `... ${arr[i]}ºC in ${i + 1} days `;
+  }
+  console.log(str + '...');
+};
+printForecast(temper1);
+
+//2, Using Test Data 2
+console.log(
+  `${temper2[0]} ... ${temper2[1]} ... ${temper2[2]} ... ${temper2[3]} ... ${temper2[4]}`
+);
+const printForecast2 = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `... ${arr[i]}ºC in ${i + 1} days `;
+  }
+  console.log(str + '...');
+};
+printForecast2(temper2);
